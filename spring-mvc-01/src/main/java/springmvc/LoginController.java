bfda0a23b3dd76b743da399b6afc6573ad19ca1c
OptimistLabyrinth <keesung0705@gmail.com>
2022-09-06 22:55:10 +0900

    Spring MVC 4 - Auto Wiring

--- spring-mvc-01/src/main/java/springmvc/LoginController.java
+++ spring-mvc-01/src/main/java/springmvc/LoginController.java
@@ -1,5 +1,6 @@
 package springmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
+    private final LoginService loginService;
+
+    @Autowired
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String showLoginPage() {
         return "login";
@@ -21,6 +29,10 @@ public class LoginController {
         ModelMap model
     ) {
         System.out.println("name: " + name + ", password: " + password);
+        if (!loginService.validateUser(name, password)) {
+            model.put("errorMessage", "invalid credentials");
+            return "login";
+        }
         model.put("name", name);
         return "welcome";
     }
